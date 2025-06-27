ea4d0592e90f08ec20b4d75a03acfbb8c089728f
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2025-06-27 19:06:20 +0530

    overdue_days updated

---
+++ server/cron/overdueCron.js
@@ -0,0 +1,23 @@
+// cron/overdueCron.js
+const cron = require('node-cron');
+const Loan = require('../models/loan.model');
+
+cron.schedule('0 0 * * *', async () => {     // every night at 00:00
+  await Loan.updateMany(
+    { loan_status: 'active', due_date: { $lte: new Date() } },
+    [
+      {
+        $set: {
+          overdue_days: {
+            $dateDiff: {
+              startDate: '$due_date',
+              endDate:   '$$NOW',
+              unit:      'day'
+            }
+          }
+        }
+      }
+    ]
+  );
+  console.log('✅ overdue_days updated');
+});
